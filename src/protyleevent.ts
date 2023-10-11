985a53dd122718cbcec500b67a320d0b46363a61
TinkMingKing <32903588+TinkMingKing@users.noreply.github.com>
2023-10-11 01:28:52 +0000

    fix 手机端自动更新无效 #40

--- src/protyleevent.ts
+++ src/protyleevent.ts
@@ -1,14 +1,17 @@
 import { insertAuto } from "./createIndex";
+import { isMobile } from "./utils";
 // import { settings } from "./settings";
 
 export function updateIndex({ detail }: any) {
-    if (detail.options.mode != undefined) {
-        // || !settings.get("autoUpdate")
-        return;
+    if (!isMobile) {
+        if(detail.options.mode != undefined){
+            // || !settings.get("autoUpdate")
+            return;
+        }
     }
     // console.log(detail);
     let notebookId = detail.notebookId;
     let path = detail.path;
-    let parentId = detail.options.blockId;
+    let parentId = detail.block.rootID;
     insertAuto(notebookId,path,parentId);
 }
